0b1cc16ff82473028a5a40aaeda7618a6d4f4c29
Minghui <00vertox@gmail.com>
2023-01-02 19:24:29 +0800

    initial commit

---
+++ gulimall-order/src/test/java/com/ke/gulimall/gulimallorder/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ke.gulimall.gulimallorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
